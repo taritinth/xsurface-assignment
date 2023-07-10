e37ecbf9e779a1720e5e0772d0b3791c2f8acd18
taritinth <tarit.khanbo@hotmail.com>
2023-07-10 13:38:26 +0700

    build: edit env variable

--- web/src/utils/axios.js
+++ web/src/utils/axios.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL:
-    import.meta.env.NODE_ENV === "production"
-      ? import.meta.env.VITE_PROD_API_URL
-      : import.meta.env.VITE_DEV_API_URL,
+  baseURL: import.meta.env.PROD
+    ? import.meta.env.VITE_PROD_API_URL
+    : import.meta.env.VITE_DEV_API_URL,
 });
 
 export { instance as axios };
